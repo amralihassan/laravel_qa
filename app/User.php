38cb582f5fb15468e1c0a0daa7e91f0f4d11b1a1
amr <amr_dotnet@ymail.com>
2020-07-08 19:48:57 +0200

    generate faker date and using eloquent event to increment answer_cout field

--- app/User.php
+++ app/User.php
@@ -41,6 +41,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Question::class);
     }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
